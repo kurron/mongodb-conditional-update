7fd612a0cccfe3db70fc78967806fa99134418b3
Ron Kurr <kurr@kurron.org>
2017-07-21 23:43:41 -0400

    Better tests, failing query.

--- src/test/groovy/org/kurron/IntegrationTest.groovy
+++ src/test/groovy/org/kurron/IntegrationTest.groovy
@@ -4,12 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.boot.test.context.TestConfiguration
-import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.FindAndModifyOptions
 import org.springframework.data.mongodb.core.MongoTemplate
-import org.springframework.data.mongodb.core.index.Index
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
+import spock.lang.Ignore
 import spock.lang.Specification
 
 import static org.springframework.data.mongodb.core.query.Criteria.where
@@ -37,7 +36,8 @@ class IntegrationTest  extends Specification {
         template.dropCollection( TestData )
         template.createCollection( TestData )
         template.indexOps( TestData ).dropAllIndexes()
-        template.indexOps( TestData ).ensureIndex( new Index().on( '_id', Sort.Direction.ASC ).on( 'fencingToken', Sort.Direction.ASC ).unique( Index.Duplicates.RETAIN ).named( 'fencingToken' ) )
+//        template.indexOps( TestData ).ensureIndex( new Index().on( '_id', Sort.Direction.ASC ).on( 'fencingToken', Sort.Direction.DESC ).unique( Index.Duplicates.RETAIN ).named( 'fencingToken' ) )
+//        template.indexOps( TestData ).ensureIndex( new Index().on( '_id', Sort.Direction.ASC ).on( 'fencingToken', Sort.Direction.DESC ).named( 'fencingToken' ) )
     }
 
     List<TestData> createData( int count = 10 ) {
@@ -46,19 +46,61 @@ class IntegrationTest  extends Specification {
         }
     }
 
+    static private List constructStatement( TestData it ) {
+        def query = new Query( where('_id').is(1 ).andOperator( where('fencingToken' ).lt( it.fencingToken ) ) )
+//        def query = new Query( where('_id').is(1 ) )
+        def update = new Update().set( 'currentState', it.fencingToken ).push( 'touchedBy', it.fencingToken ).setOnInsert( 'fencingToken', it.fencingToken )
+        def options = new FindAndModifyOptions().upsert(true ).returnNew(true )
+        [query, update, options]
+    }
+
     def 'sequential processing'() {
         given: 'sequential test data'
         def data = createData()
 
         when: 'data is saved to the database'
         def updates = data.collect {
-            def query = new Query( where( '_id' ).is( 1 ).andOperator( where( 'fencingToken' ).lt( it.fencingToken ) ) )
-            def update = new Update().set( 'currentState', it.fencingToken ).push( 'touchedBy', it.fencingToken ).inc( 'fencingToken', 1 )
-            def options = new FindAndModifyOptions().upsert( true ).returnNew( true )
+            def (Query query, Update update, FindAndModifyOptions options) = constructStatement( it )
             template.findAndModify( query, update, options, TestData )
         }
 
         then: 'state in the database matches expectations'
-        updates.currentState.max() == data.currentState.max()
+        def query = new Query( where('_id').is(1 ) )
+        def inDatabase = template.findOne( query, TestData )
+        inDatabase.currentState == data.last().currentState
+    }
+
+    def 'reverse processing'() {
+        given: 'reversed test data'
+        def data = createData().reverse()
+
+        when: 'data is saved to the database'
+        def updates = data.collect {
+            def (Query query, Update update, FindAndModifyOptions options) = constructStatement( it )
+            template.findAndModify( query, update, options, TestData )
+        }
+
+        then: 'state in the database matches expectations'
+        def query = new Query( where('_id').is(1 ) )
+        def inDatabase = template.findOne( query, TestData )
+        inDatabase.currentState == data.first().currentState
+    }
+
+    @Ignore
+    def 'random processing'() {
+        given: 'randomized test data'
+        def data = createData()
+        Collections.shuffle( data )
+
+        when: 'data is saved to the database'
+        def updates = data.collect {
+            def (Query query, Update update, FindAndModifyOptions options) = constructStatement( it )
+            template.findAndModify( query, update, options, TestData )
+        }
+
+        then: 'state in the database matches expectations'
+        def query = new Query( where('_id').is(1 ) )
+        def inDatabase = template.findOne( query, TestData )
+        inDatabase.currentState == data.currentState.max()
     }
 }
