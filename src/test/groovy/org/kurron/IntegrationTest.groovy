52da5339a4ef6826510056b41fd2acf620b979b1
Ron Kurr <kurr@kurron.org>
2017-07-22 00:16:03 -0400

    I think I got it!

--- src/test/groovy/org/kurron/IntegrationTest.groovy
+++ src/test/groovy/org/kurron/IntegrationTest.groovy
@@ -4,11 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.FindAndModifyOptions
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.data.mongodb.core.index.Index
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
-import spock.lang.Ignore
 import spock.lang.Specification
 
 import static org.springframework.data.mongodb.core.query.Criteria.where
@@ -36,7 +37,7 @@ class IntegrationTest  extends Specification {
         template.dropCollection( TestData )
         template.createCollection( TestData )
         template.indexOps( TestData ).dropAllIndexes()
-//        template.indexOps( TestData ).ensureIndex( new Index().on( '_id', Sort.Direction.ASC ).on( 'fencingToken', Sort.Direction.DESC ).unique( Index.Duplicates.RETAIN ).named( 'fencingToken' ) )
+        template.indexOps( TestData ).ensureIndex( new Index().on( '_id', Sort.Direction.ASC ).on( 'fencingToken', Sort.Direction.DESC ).unique( Index.Duplicates.RETAIN ).named( 'fencingToken' ) )
 //        template.indexOps( TestData ).ensureIndex( new Index().on( '_id', Sort.Direction.ASC ).on( 'fencingToken', Sort.Direction.DESC ).named( 'fencingToken' ) )
     }
 
@@ -46,12 +47,37 @@ class IntegrationTest  extends Specification {
         }
     }
 
-    static private List constructStatement( TestData it ) {
+    /*
+    So here is the problem, when the query fails, an upsert is performed, event when it fails
+    because the fending token is the cause.  The first time through, this is fine because it it
+    an insert.  The next time through, the upsert tries to kick in and you get a duplicate key
+    exception.  Unfortunately, MongoDB really wants us to split the insert from the update,
+    which ie exactly what we are trying to avoid.  Could we do it in two stages?
+     */
+    static private List constructStatement( TestData it, boolean upsert = true ) {
         def query = new Query( where('_id').is(1 ).andOperator( where('fencingToken' ).lt( it.fencingToken ) ) )
 //        def query = new Query( where('_id').is(1 ) )
-        def update = new Update().set( 'currentState', it.fencingToken ).push( 'touchedBy', it.fencingToken ).setOnInsert( 'fencingToken', it.fencingToken )
-        def options = new FindAndModifyOptions().upsert(true ).returnNew(true )
+        def update = new Update().set( 'currentState', it.fencingToken ).push( 'touchedBy', it.fencingToken ).set( 'fencingToken', it.fencingToken )
+        def options = new FindAndModifyOptions().upsert(upsert ).returnNew(true )
         [query, update, options]
+    }
+
+    private TestData loadCurrentDocument() {
+        def query = new Query( where('_id' ).is(1 ) )
+        template.findOne(query, TestData)
+    }
+
+    private List<TestData> saveToDatabase(List<TestData> data) {
+        data.collect {
+            try {
+                def (Query query, Update update, FindAndModifyOptions options) = constructStatement(it)
+                template.findAndModify(query, update, options, TestData)
+            }
+            catch (DuplicateKeyException) {
+                def (Query query, Update update, FindAndModifyOptions options) = constructStatement(it, false)
+                template.findAndModify(query, update, options, TestData)
+            }
+        }
     }
 
     def 'sequential processing'() {
@@ -59,14 +85,10 @@ class IntegrationTest  extends Specification {
         def data = createData()
 
         when: 'data is saved to the database'
-        def updates = data.collect {
-            def (Query query, Update update, FindAndModifyOptions options) = constructStatement( it )
-            template.findAndModify( query, update, options, TestData )
-        }
+        saveToDatabase(data)
 
         then: 'state in the database matches expectations'
-        def query = new Query( where('_id').is(1 ) )
-        def inDatabase = template.findOne( query, TestData )
+        def inDatabase = loadCurrentDocument()
         inDatabase.currentState == data.last().currentState
     }
 
@@ -75,32 +97,23 @@ class IntegrationTest  extends Specification {
         def data = createData().reverse()
 
         when: 'data is saved to the database'
-        def updates = data.collect {
-            def (Query query, Update update, FindAndModifyOptions options) = constructStatement( it )
-            template.findAndModify( query, update, options, TestData )
-        }
+        saveToDatabase(data)
 
         then: 'state in the database matches expectations'
-        def query = new Query( where('_id').is(1 ) )
-        def inDatabase = template.findOne( query, TestData )
+        def inDatabase = loadCurrentDocument()
         inDatabase.currentState == data.first().currentState
     }
 
-    @Ignore
     def 'random processing'() {
         given: 'randomized test data'
         def data = createData()
         Collections.shuffle( data )
 
         when: 'data is saved to the database'
-        def updates = data.collect {
-            def (Query query, Update update, FindAndModifyOptions options) = constructStatement( it )
-            template.findAndModify( query, update, options, TestData )
-        }
+        saveToDatabase(data)
 
         then: 'state in the database matches expectations'
-        def query = new Query( where('_id').is(1 ) )
-        def inDatabase = template.findOne( query, TestData )
+        def inDatabase = loadCurrentDocument()
         inDatabase.currentState == data.currentState.max()
     }
 }
