9d60c6926e78d015d3f5c67fafc4afa660753675
Ron Kurr <kurr@kurron.org>
2017-07-21 20:11:14 -0400

    Integration test comes up.

--- src/test/groovy/org/kurron/IntegrationTest.groovy
+++ src/test/groovy/org/kurron/IntegrationTest.groovy
@@ -1,11 +1,29 @@
 package org.kurron
 
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.data.mongodb.core.MongoTemplate
 import spock.lang.Specification
 
 /**
  * Integration test to see how MongoDB conditional updates behave.
  */
+@DataMongoTest
 class IntegrationTest  extends Specification {
+
+    @Autowired
+    private MongoTemplate template
+
+
+    @TestConfiguration
+    static class Configuration {
+
+    }
+
+    def setup() {
+        assert template
+    }
 
     def 'sequential processing'() {
         given:
@@ -15,6 +33,6 @@ class IntegrationTest  extends Specification {
         'bar'
 
         then:
-        false
+        true
     }
 }
